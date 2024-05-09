b89dfb583770c3f5477129d859bfad088be4a354
MinhHuy20 <minhhuy141204@gmail.com>
2024-05-09 14:52:18 +0700

    Bài tập buổi 01

---
+++ Bai01/bai1.js
@@ -0,0 +1,9 @@
+let mssv1 = 2122110547
+let hoten1 = "DongMinhHuy"
+console.log(mssv1)
+console.log(hoten1)
+
+document.getElementById("show").innerHTML = `
+MSSV: ${mssv1} <br>
+Ho ten: ${hoten1}
+`;
---
+++ Bai02/bai2.js
@@ -0,0 +1,16 @@
+
+document.getElementById("show").innerHTML = `
+<tr>
+<td>1</td>
+<td>2122110547</td>
+<td>Đồng Minh Huy</td>
+<td>CCQ2211M</td>
+</tr>
+
+<tr>
+<td>2</td>
+<td>2122110544</td>
+<td>Võ Đức Thuận</td>
+<td>CCQ2211M</td>
+</tr>
+`;
